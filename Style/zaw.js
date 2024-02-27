0873b56921d2f4837683cb5d48f00667bf1fd840
Rampardos <63419320+BarbatosV2@users.noreply.github.com>
2024-02-28 02:11:11 +1030

    updated JS

--- Style/zaw.js
+++ Style/zaw.js
@@ -14,4 +14,8 @@ function toggleTheme() {
     const theme = body.classList.contains('dark-theme') ? 'dark' : 'light';
     githubIcon.src = `Logos/Github-Logos/github-mark-${theme}.png`;
 
+    // Change the copyright text color based on the theme
+    const copyrightText = document.querySelector('#copyright p');
+    const textColor = body.classList.contains('dark-theme') ? 'rgba(255, 255, 255, 0.7)' : 'rgba(0, 0, 0, 0.7)';
+    copyrightText.style.color = textColor;
 }
